9b3f4cb69398c97d5d5f9a8ca078a6510aaa893b
duyhuy25 <duyhuy1342005@gmail.com>
2026-09-08 08:39:36 +0700

    add điều hướng các file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { AppHeader } from '@/components/ui/app-header';
 import { Footer } from '@/components/ui/footer';
+import { useFooterActions } from '@/constants/footer-actions';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+  const footerActions = useFooterActions('home');
+
   return (
     <SafeAreaView style={styles.safeArea} edges={['top', 'left', 'right']}>
       <View style={styles.container}>
@@ -12,15 +15,7 @@ export default function HomeScreen() {
         <View style={styles.content}>
           <Text style={styles.text}>Nội dung chính</Text>
         </View>
-        <Footer
-          actions={[
-            { title: 'Trang chủ', icon: 'house.fill', active: true, onPress: () => console.log('Trang chủ') },
-            { title: 'Tìm kiếm', icon: 'magnifyingglass', onPress: () => console.log('Tìm kiếm') },
-            { title: 'Thư viện', icon: 'books.vertical', onPress: () => console.log('Thư viện') },
-            { title: 'Đăng ký', icon: 'music.note', onPress: () => console.log('Login') },
-            { title: 'Tạo', icon: 'plus', onPress: () => console.log('Tạo') },
-          ]}
-        />
+        <Footer actions={footerActions} />
       </View>
     </SafeAreaView>
   );
